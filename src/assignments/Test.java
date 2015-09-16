c3b68c72d816417462a41b10a6a38b555d62d479
nerq <hr.nicknielsen@gmail.com>
2015-09-16 15:12:26 +0200

    Mastrubation feels good! #VIERDERSNART!

--- src/assignments/Test.java
+++ src/assignments/Test.java
@@ -3,9 +3,15 @@ package assignments;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.Map;
 import java.util.Map.Entry;
 
@@ -31,76 +37,99 @@ public class Test {
 		
 		//
 		HashMap<GeoPosition, Map<MACAddress, Double>> onlineMap = new HashMap<GeoPosition, Map<MACAddress, Double>>();
-		for(TraceEntry entry: onlineTrace){
-			for(MACAddress address: entry.getSignalStrengthSamples().getSortedAccessPoints()){
-				HashMap<MACAddress, Double> mapUsingMac = new HashMap<MACAddress, Double>();
-				
-				mapUsingMac.put(MACAddress.parse("00:14:BF:B1:7C:54"), (double) -100);
-				mapUsingMac.put(MACAddress.parse("00:16:B6:B7:5D:8F"), (double) -100);
-				mapUsingMac.put(MACAddress.parse("00:14:BF:B1:7C:57"), (double) -100);
-				mapUsingMac.put(MACAddress.parse("00:14:BF:B1:97:8D"), (double) -100);
-				mapUsingMac.put(MACAddress.parse("00:16:B6:B7:5D:9B"), (double) -100);
-				mapUsingMac.put(MACAddress.parse("00:14:6C:62:CA:A4"), (double) -100);
-				mapUsingMac.put(MACAddress.parse("00:14:BF:3B:C7:C6"), (double) -100);
-				mapUsingMac.put(MACAddress.parse("00:14:BF:B1:97:8A"), (double) -100);
-				mapUsingMac.put(MACAddress.parse("00:14:BF:B1:97:81"), (double) -100);
-				mapUsingMac.put(MACAddress.parse("00:16:B6:B7:5D:8C"), (double) -100);
-				mapUsingMac.put(MACAddress.parse("00:11:88:28:5E:E0"), (double) -100);
-				
-				if(mapUsingMac.containsKey(address)){
-					mapUsingMac.put(address, entry.getSignalStrengthSamples().getAverageSignalStrength(address));
-				}
-				onlineMap.put(entry.getGeoPosition(), mapUsingMac);
+		for(TraceEntry entry: onlineTrace){			
+			HashMap<MACAddress, Double> knownMac = new HashMap<MACAddress, Double>(){{
+				put(MACAddress.parse("00:11:88:28:5E:E0"), (double) -100); 
+				put(MACAddress.parse("00:16:B6:B7:5D:8C"), (double) -100);
+				put(MACAddress.parse("00:14:BF:B1:97:81"), (double) -100);
+				put(MACAddress.parse("00:14:BF:B1:97:8A"), (double) -100);
+				put(MACAddress.parse("00:14:BF:3B:C7:C6"), (double) -100);
+				put(MACAddress.parse("00:14:6C:62:CA:A4"), (double) -100);
+				put(MACAddress.parse("00:16:B6:B7:5D:9B"), (double) -100);
+				put(MACAddress.parse("00:14:BF:B1:97:8D"), (double) -100);
+				put(MACAddress.parse("00:14:BF:B1:7C:57"), (double) -100);
+				put(MACAddress.parse("00:16:B6:B7:5D:8F"), (double) -100);
+				put(MACAddress.parse("00:14:BF:B1:7C:54"), (double) -100);
+			}};
+			LinkedList<MACAddress> listOfMacInTrace = new LinkedList<MACAddress>();
+				listOfMacInTrace.addAll(entry.getSignalStrengthSamples().getSortedAccessPoints());
+			Iterator<MACAddress> listIterator = listOfMacInTrace.iterator();
+			while(listIterator.hasNext()){
+					MACAddress currentMac = listIterator.next();
+					if(knownMac.containsKey(currentMac)){
+						knownMac.put(currentMac, entry.getSignalStrengthSamples().getAverageSignalStrength(currentMac));
+					}
 			}
+			onlineMap.put(entry.getGeoPosition(), knownMac);
 		}
 		
 		HashMap<GeoPosition, Map<MACAddress, Double>> offlineMap = new HashMap<GeoPosition, Map<MACAddress, Double>>();
-		for(TraceEntry entry: offlineTrace){
-			for(MACAddress address: entry.getSignalStrengthSamples().getSortedAccessPoints()){
-				HashMap<MACAddress, Double> mapUsingMac = new HashMap<MACAddress, Double>();
-				
-				mapUsingMac.put(MACAddress.parse("00:14:BF:B1:7C:54"), (double) -100);
-				mapUsingMac.put(MACAddress.parse("00:16:B6:B7:5D:8F"), (double) -100);
-				mapUsingMac.put(MACAddress.parse("00:14:BF:B1:7C:57"), (double) -100);
-				mapUsingMac.put(MACAddress.parse("00:14:BF:B1:97:8D"), (double) -100);
-				mapUsingMac.put(MACAddress.parse("00:16:B6:B7:5D:9B"), (double) -100);
-				mapUsingMac.put(MACAddress.parse("00:14:6C:62:CA:A4"), (double) -100);
-				mapUsingMac.put(MACAddress.parse("00:14:BF:3B:C7:C6"), (double) -100);
-				mapUsingMac.put(MACAddress.parse("00:14:BF:B1:97:8A"), (double) -100);
-				mapUsingMac.put(MACAddress.parse("00:14:BF:B1:97:81"), (double) -100);
-				mapUsingMac.put(MACAddress.parse("00:16:B6:B7:5D:8C"), (double) -100);
-				mapUsingMac.put(MACAddress.parse("00:11:88:28:5E:E0"), (double) -100);
-				
-				if(mapUsingMac.containsKey(address)){
-					mapUsingMac.put(address, entry.getSignalStrengthSamples().getAverageSignalStrength(address));
-				}
-				offlineMap.put(entry.getGeoPosition(), mapUsingMac);
+		for(TraceEntry entry: offlineTrace){			
+			HashMap<MACAddress, Double> knownMac = new HashMap<MACAddress, Double>(){{
+				put(MACAddress.parse("00:11:88:28:5E:E0"), (double) -100); 
+				put(MACAddress.parse("00:16:B6:B7:5D:8C"), (double) -100);
+				put(MACAddress.parse("00:14:BF:B1:97:81"), (double) -100);
+				put(MACAddress.parse("00:14:BF:B1:97:8A"), (double) -100);
+				put(MACAddress.parse("00:14:BF:3B:C7:C6"), (double) -100);
+				put(MACAddress.parse("00:14:6C:62:CA:A4"), (double) -100);
+				put(MACAddress.parse("00:16:B6:B7:5D:9B"), (double) -100);
+				put(MACAddress.parse("00:14:BF:B1:97:8D"), (double) -100);
+				put(MACAddress.parse("00:14:BF:B1:7C:57"), (double) -100);
+				put(MACAddress.parse("00:16:B6:B7:5D:8F"), (double) -100);
+				put(MACAddress.parse("00:14:BF:B1:7C:54"), (double) -100);
+			}};
+			LinkedList<MACAddress> listOfMacInTrace = new LinkedList<MACAddress>();
+				listOfMacInTrace.addAll(entry.getSignalStrengthSamples().getSortedAccessPoints());
+			Iterator<MACAddress> listIterator = listOfMacInTrace.iterator();
+			while(listIterator.hasNext()){
+					MACAddress currentMac = listIterator.next();
+					if(knownMac.containsKey(currentMac)){
+						knownMac.put(currentMac, entry.getSignalStrengthSamples().getAverageSignalStrength(currentMac));
+					}
 			}
+			offlineMap.put(entry.getGeoPosition(), knownMac);
 		}
 		
-		
+		HashMap<GeoPosition, Map<GeoPosition, Double>> distances = new HashMap<GeoPosition, Map<GeoPosition, Double>>();
 		for(Entry<GeoPosition, Map<MACAddress, Double>> value : onlineMap.entrySet()){
 			ArrayList<Double> onlineValues = new ArrayList<Double>();
 			for(Entry<MACAddress, Double> value2 : value.getValue().entrySet()){
 				onlineValues.add(value2.getValue());
 			}
+			HashMap<GeoPosition, Double> innerDistances = new HashMap<GeoPosition, Double>();
 			for(Entry<GeoPosition, Map<MACAddress, Double>> value3 : offlineMap.entrySet()){
 				ArrayList<Double> offlineValues = new ArrayList<Double>();
 				for(Entry<MACAddress, Double> value4 : value3.getValue().entrySet()){
 					offlineValues.add(value4.getValue());
 					}
-				System.out.println(Helpers.EuclidianDistanceAll(onlineValues, offlineValues));
-			}
+				innerDistances.put(value3.getKey(), Helpers.EuclidianDistanceAll(onlineValues, offlineValues));
+				}
+			innerDistances = sortByValues(innerDistances);
+			distances.put(value.getKey(), innerDistances);
 		}
-		//tag et online entry
-			//sorter for signalstyrker (tilføj -100 til dem der mangler)
-		//hent ét offline entry
-			//sorter for signalstyrker (tilføj -100 til dem der mangler)
-		//lav matematik
-			//gem geopos + distance i HashMap<Double,GeoPos>
-		//REPEAT for alle online entries
-		//Sort efter HashMap efter distance og pluk k antal ud. 
-		//lav k -nearest formel, hvis k>1
+		
+		System.out.println(distances.values().iterator().next());
+		
 	}
+	
+	private static HashMap sortByValues(HashMap map) { 
+	       List list = new LinkedList(map.entrySet());
+	       // Defined Custom Comparator here
+	       Collections.sort(list, new Comparator() {
+	            public int compare(Object o1, Object o2) {
+	               return ((Comparable) ((Map.Entry) (o1)).getValue())
+	                  .compareTo(((Map.Entry) (o2)).getValue());
+	            }
+	       });
+
+	       // Here I am copying the sorted list in HashMap
+	       // using LinkedHashMap to preserve the insertion order
+	       HashMap sortedHashMap = new LinkedHashMap();
+	       for (Iterator it = list.iterator(); it.hasNext();) {
+	              Map.Entry entry = (Map.Entry) it.next();
+	              sortedHashMap.put(entry.getKey(), entry.getValue());
+	       } 
+	       return sortedHashMap;
+	  }
 		
 }
